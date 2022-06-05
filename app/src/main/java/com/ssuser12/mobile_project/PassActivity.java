d0f8ec5a24349a311ce1ab265419e888ac87e50f
soojin <soojin110311@naver.com>
2022-06-05 17:21:46 +0900

    fourth

--- app/src/main/java/com/ssuser12/mobile_project/PassActivity.java
+++ app/src/main/java/com/ssuser12/mobile_project/PassActivity.java
@@ -1,4 +1,32 @@
 package com.ssuser12.mobile_project;
 
-public class PassActivity extends MainActivity{
-}
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class PassActivity extends MainActivity {
+    Button btn1;
+    EditText edt1,edt2,edt3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass);
+        edt1=findViewById(R.id.edt1);
+        edt2=findViewById(R.id.edt2);
+        edt3=findViewById(R.id.edt3);
+        btn1=findViewById(R.id.btn1);
+
+     btn1.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             if(edt2.equals(edt3)) {
+                 Intent intent = new Intent(getApplicationContext(), RealActivity.class);
+                 startActivity(intent);
+             }
+            }
+         });
+          }
+    }
+
---
+++ app/src/main/java/com/ssuser12/mobile_project/RealActivity.java
@@ -0,0 +1,11 @@
+package com.ssuser12.mobile_project;
+
+import android.os.Bundle;
+
+public class RealActivity extends MainActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real);
+    }
+}
